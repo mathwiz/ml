b0e806bc63b24fd759d8585b0346f2444e493e1f
Yohan Lee <mathwiz@gmail.com>
2019-06-12 16:21:03 -0400

    neuron

--- neuralnet/neuron_test.py
+++ neuralnet/neuron_test.py
@@ -1,5 +1,33 @@
+import unittest
 from neuron import *
 
 
-print(ReLU(-1))
-print(ReLU(1))
+def even(x):
+    return x % 2 == 0
+
+
+def expt_inv(b, p, a):
+    if p == 0:
+        return a
+    elif even(p):
+        return expt_inv(b * b, p / 2, a)
+    else:
+        return expt_inv(b, p - 1, a * b)
+
+
+def expt(b, p):
+    return expt_inv(b, p, 1)
+
+
+class MyTestCase(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(ReLU(1), 1)
+        self.assertEqual(ReLU(0), 0)
+        self.assertEqual(ReLU(-1), 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
